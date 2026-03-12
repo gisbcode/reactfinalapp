28e58ca62561d57f17fca57769d77105ba750773
Gigi <creative.giovannasb@gmail.com>
2026-03-12 21:36:25 +0000

    spaces

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by
+        This project was coded by{" "}
         <a href="https://github.com/gisbcode" target="blank">
           Giovanna Bellon
-        </a>
+        </a>{" "}
         and is open sourced on{" "}
         <a href="https://github.com/gisbcode/reactfinalapp" target="blank">
           GitHub
