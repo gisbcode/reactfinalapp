9de261284af4fffade584693090116f63da43930
Gigi <creative.giovannasb@gmail.com>
2026-03-14 21:09:44 +0000

    Create weather.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/gisbcode" target="blank">
-          Giovanna Bellon
-        </a>{" "}
-        and is open sourced on{" "}
-        <a href="https://github.com/gisbcode/reactfinalapp" target="blank">
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://reactfinalappgio.netlify.app/" target="blank">
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/gisbcode" target="blank">
+            Giovanna Bellon
+          </a>{" "}
+          and is open sourced on{" "}
+          <a href="https://github.com/gisbcode/reactfinalapp" target="blank">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://reactfinalappgio.netlify.app/" target="blank">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+export default function Weather() {
+    return (
+        <div className="Weather">Hello APP Weather</div>
+    )
+}
